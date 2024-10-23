8975da5731349417aa6bb1ba6ea1474e90f6656f
shravya <shravya.t@egradtutor.in>
2024-10-23 17:05:33 +0530

    New commit(23-20-24)

---
+++ ExamsDocument_upload/backend/database.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2/promise');
+ 
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "upload_documents",
+  waitForConnections: true,
+  connectionLimit: 1000,
+  queueLimit: 0,
+});
+ 
+// Attempt to get a connection from the pool
+pool.getConnection()
+  .then(connection => {
+    console.log('Connected to MySQL db2');
+    connection.release(); // Release the connection back to the pool
+  })
+  .catch(err => {
+    console.error('Error connecting to MySQL:', err);
+  });
+ 
+module.exports = pool;
---
+++ ExamsDocument_upload/backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const port = 5000;
+// CORS configuration
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    credentials:true,
+    
+}));
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        cb(null, `${Date.now()}${ext}`);
+    }
+});
+const upload = multer({ storage });
+
+const ExamCreation = require('./ExamCreation');
+app.use('/ExamCreation', ExamCreation);
+
+//start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
