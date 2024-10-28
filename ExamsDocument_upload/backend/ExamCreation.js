cefcc9e781e5b010cbfad4041542b51bb7048a6e
sivabhargava2024 <163115354+sivabhargava2024@users.noreply.github.com>
2024-10-28 10:15:27 +0530

    cmit pending qlevel and questions table insertion

--- ExamsDocument_upload/backend/ExamCreation.js
+++ ExamsDocument_upload/backend/ExamCreation.js
@@ -35,7 +35,7 @@ router.get('/subjectsForExams/:exam_id', async (req, res) => {
             WHERE es.exam_id = ?`;
 
         const [results] = await db.query(query, [exam_id]);
-        
+
         res.json(results);
     } catch (err) {
         console.error('Error:', err);
@@ -47,144 +47,16 @@ router.get('/subjectsForExams/:exam_id', async (req, res) => {
 
 
 
-// // Helper function to insert records into the database
-// async function insertRecord(document, data) {
-//     const [result] = await db.query(`INSERT INTO ${document} SET ?`, data);
-//     return result.insertId;
-// }
-
-// router.post('/uploadDocument', upload.single('document'), async (req, res) => {
-//     const outputDir = path.join(__dirname, 'uploads');
-//     const filePath = req.file.path;
-
-//     try {
-//         await fsPromises.mkdir(outputDir, { recursive: true });
-
-//         const result = await mammoth.convertToHtml({ path: filePath });
-//         const htmlContent = result.value;
-//         const textResult = await mammoth.extractRawText({ path: filePath });
-//         const textContent = textResult.value;
-
-//         const textSections = textContent.split('\n\n');
-//         const { selectedSubjects } = req.body;
-//         const documentName = req.file.originalname;
-
-//         const documentResult = await insertRecord('documents', {
-//             doc_name: documentName,
-//             subject_id: selectedSubjects
-//         });
-//         const documentId = documentResult;
-
-//         const images = [];
-//         const $ = cheerio.load(htmlContent);
-//         $('img').each((i, element) => {
-//             const base64Data = $(element).attr('src').replace(/^data:image\/\w+;base64,/, '');
-//             if (base64Data) {
-//                 const imageBuffer = Buffer.from(base64Data, 'base64');
-//                 images.push(imageBuffer);
-//             }
-//         });
-
-//         let question_code = '';
-//         let question_id = 0;
-//         let solution_id = 0;
-//         let imageIndex = 0;
-//         let qnlevel_id=0;
-//         let k = 1;
-
-//         for (let i = 0; i < textSections.length; i++) {
-//             if (textSections[i].includes('[QID]')) {
-//                 question_code = textSections[i].replace('[QID]', '').trim();
-//                 const questionCodeData = { question_code, doc_id: documentId };
-//                 await insertRecord('question_code_table', questionCodeData);
-//             } 
-//             else if (textSections[i].includes('[Q]')) {
-//                 let questionImagePath = '';
-//                 if (imageIndex < images.length) {
-//                     const imageName = `snapshot_${documentId}_question_${k}.png`;
-//                     const imagePath = path.join(outputDir, imageName);
-//                     await fsPromises.writeFile(imagePath, images[imageIndex]);
-//                     questionImagePath = imageName;
-//                     imageIndex++;
-//                     k++;
-//                 }
-//                 const questionData = { question_code, question_img: questionImagePath, subject_id: selectedSubjects,qnlevel_id:qnlevel_id };
-//                 question_id = await insertRecord('questions', questionData);
-//             } else if (textSections[i].includes('[QANS]')) {
-//                 const answerText = textSections[i].replace('[QANS]', '').trim();
-//                 const answerData = { answer_text: answerText, question_id: question_id };
-//                 await insertRecord('answers', answerData);
-//             } 
-//             else if (textSections[i].includes('[QTYPE]')) {
-//                 const questionType = textSections[i].replace('[QTYPE]', '').trim();
-//                 const questionTypeData = { type_of_question: questionType };
-//                 await insertRecord('question_type', questionTypeData);
-//             }
-//              else if (textSections[i].includes('[QSOL]')) {
-//                 let solutionImagePath = '';
-//                 if (imageIndex < images.length) {
-//                     const imageName = `snapshot_${documentId}_solution_${k}.png`;
-//                     const imagePath = path.join(outputDir, imageName);
-//                     await fsPromises.writeFile(imagePath, images[imageIndex]);
-//                     solutionImagePath = imageName;
-//                     imageIndex++;
-//                     k++;
-//                 }
-//                 const solutionData = { question_id: question_id, solution_img: solutionImagePath };
-//                 solution_id = await insertRecord('solution', solutionData);
-//             }
-            
-//             else if (textSections[i].includes('[QVSOL]')) {
-//                 if (solution_id) {
-//                     const solution_link = textSections[i].replace('[QVSOL]', '').trim();
-//                     const videoSolutionData = { solution_id: solution_id, video_sol_link: solution_link };
-//                     await insertRecord('video_solution', videoSolutionData);
-//                 } else {
-//                     console.error('Solution ID not found for video solution');
-//                 }
-//             }
-//             else if (textSections[i].includes('[QL]')) {
-//                 if (question_id) {
-//                     const qnlevel_name = textSections[i].replace('[QL]', '').trim();
-//                     const questionLevelData = { 
-//                         question_id:question_id,
-//                         qnlevel_name:qnlevel_name
-//                      };
-//                     await insertRecord('question_level', questionLevelData);
-//                 } else {
-//                     console.error('Question level ID not found for question');
-//                 }
-//             }
-//             else if (['(a)', '(b)', '(c)', '(d)'].some(option => textSections[i].startsWith(option))) {
-//                 const optionText = textSections[i].trim();
-//                 let optionImagePath = '';
-//                 if (imageIndex < images.length) {
-//                     const imageName = `snapshot_${documentId}_option_${k}.png`;
-//                     const imagePath = path.join(outputDir, imageName);
-//                     await fsPromises.writeFile(imagePath, images[imageIndex]);
-//                     optionImagePath = imageName;
-//                     imageIndex++;
-//                     k++;
-//                 }
-//                 const optionData = { question_id: question_id, option_index: optionText, option_img: optionImagePath };
-//                 await insertRecord('options', optionData);
-//             }
-//         }
-
-//         res.send('Document uploaded and processed successfully.');
-//     } catch (error) {
-//         console.error('Error during document processing:', error);
-//         res.status(500).send('Failed to upload and process the document.');
-//     }
-// });
 
 async function insertRecord(document, data) {
     const [result] = await db.query(`INSERT INTO ${document} SET ?`, data);
     return result.insertId;
 }
+
 router.post('/uploadDocument', upload.single('document'), async (req, res) => {
     const outputDir = path.join(__dirname, 'uploads');
     const filePath = req.file.path;
+
 
     try {
         await fsPromises.mkdir(outputDir, { recursive: true });
@@ -197,10 +69,17 @@ router.post('/uploadDocument', upload.single('document'), async (req, res) => {
         const textSections = textContent.split('\n\n');
         const { selectedSubjects } = req.body;
         const documentName = req.file.originalname;
+        const topicName = req.body.topicName;
+
+        const topicId = await insertRecord('topics', {
+            subject_id: selectedSubjects,
+            topic_name: topicName, 
+        });
 
         const documentResult = await insertRecord('documents', {
             doc_name: documentName,
-            subject_id: selectedSubjects
+            subject_id: selectedSubjects,
+            topic_id: topicId,
         });
         const documentId = documentResult;
 
@@ -218,7 +97,7 @@ router.post('/uploadDocument', upload.single('document'), async (req, res) => {
         let question_id = 0;
         let solution_id = 0;
         let imageIndex = 0;
-        let qnlevel_id = null; // Initialize to null so it can be set when `[QL]` is encountered
+        let qnlevel_id = null; 
         let k = 1;
 
         for (let i = 0; i < textSections.length; i++) {
@@ -226,7 +105,7 @@ router.post('/uploadDocument', upload.single('document'), async (req, res) => {
                 question_code = textSections[i].replace('[QID]', '').trim();
                 const questionCodeData = { question_code, doc_id: documentId };
                 await insertRecord('question_code_table', questionCodeData);
-            } 
+            }
             else if (textSections[i].includes('[Q]')) {
                 let questionImagePath = '';
                 if (imageIndex < images.length) {
@@ -237,16 +116,16 @@ router.post('/uploadDocument', upload.single('document'), async (req, res) => {
                     imageIndex++;
                     k++;
                 }
-                const questionData = { question_code, question_img: questionImagePath, subject_id: selectedSubjects, qnlevel_id };
+                const questionData = { question_code, question_img: questionImagePath, subject_id: selectedSubjects, qnlevel_id:qnlevel_id };
                 question_id = await insertRecord('questions', questionData);
-            } 
+            }
             else if (textSections[i].includes('[QL]')) {
                 const qnlevel_name = textSections[i].replace('[QL]', '').trim();
-                
+
                 // Insert question level if available, and retrieve the ID for the level
-                const questionLevelData = { qnlevel_name,question_id:question_id};
+                const questionLevelData = { qnlevel_name};
                 const questionLevelResult = await insertRecord('question_level', questionLevelData);
-                
+
                 // Set qnlevel_id from the inserted record
                 qnlevel_id = questionLevelResult;
             }
@@ -254,13 +133,13 @@ router.post('/uploadDocument', upload.single('document'), async (req, res) => {
                 const answerText = textSections[i].replace('[QANS]', '').trim();
                 const answerData = { answer_text: answerText, question_id: question_id };
                 await insertRecord('answers', answerData);
-            } 
+            }
             else if (textSections[i].includes('[QTYPE]')) {
                 const questionType = textSections[i].replace('[QTYPE]', '').trim();
                 const questionTypeData = { type_of_question: questionType };
                 await insertRecord('question_type', questionTypeData);
             }
-             else if (textSections[i].includes('[QSOL]')) {
+            else if (textSections[i].includes('[QSOL]')) {
                 let solutionImagePath = '';
                 if (imageIndex < images.length) {
                     const imageName = `snapshot_${documentId}_solution_${k}.png`;
@@ -304,6 +183,13 @@ router.post('/uploadDocument', upload.single('document'), async (req, res) => {
         res.status(500).send('Failed to upload and process the document.');
     }
 });
+
+
+
+
+
+
+// start work on question and question level table monday
 
 
 
--- ExamsDocument_upload/backend/frontend/src/components/ExamCreation.jsx
+++ ExamsDocument_upload/backend/frontend/src/components/ExamCreation.jsx
@@ -1,4 +1,218 @@
 
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import './MainsForm.css';
+// import { IoMdHome } from "react-icons/io";
+// import Logo_img from '../Images/egate-logo.png';
+// import Leftnavbar from './Leftnavbar';
+// import { RxCross2 } from "react-icons/rx";
+
+// const ExamCreation = () => {
+//   const [modalOpen, setModalOpen] = useState(false);
+//   const [exams, setExams] = useState([]);
+//   const [subjects, setSubjects] = useState([]);
+//   const [selectedExam, setSelectedExam] = useState('');
+//   const [selectedSubjects, setSelectedSubjects] = useState([]);
+
+
+//   useEffect(() => {
+//     fetch('http://localhost:5000/ExamCreation/exams')
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             return response.json();
+//         })
+//         .then(data => setExams(data))
+//         .catch(error => console.error('Error fetching exams:', error));
+// }, []); 
+
+// useEffect(() => {
+//     if (selectedExam) {
+//         fetch(`http://localhost:5000/ExamCreation/subjectsForExams/${selectedExam}`)
+//             .then(response => {
+//                 if (!response.ok) {
+//                     throw new Error('Network response was not ok');
+//                 }
+//                 return response.json();
+//             })
+//             .then(data => setSubjects(data))
+//             .catch(error => console.error('Error fetching subjects:', error));
+//     } else {
+//         setSubjects([]);
+//     }
+// }, [selectedExam]); 
+
+
+//   const handleExamChange = (event) => {
+//     setSelectedExam(event.target.value);
+//     setSelectedSubjects([]);
+//   };
+
+//   const handleSubjectChange = (event) => {
+//     console.log(event.target.value)
+//     // const selectedOptions = Array.from(event.target.value);
+//     setSelectedSubjects(event.target.value);
+//   };
+
+
+//   const resetForm = () => {
+//     setModalOpen(false);
+//     setSelectedExam('');
+//     setSelectedSubjects([]);
+//   };
+
+//   const handleSubmit = async (event) => {
+//     console.log("submit clicked");
+//     event.preventDefault();
+
+//     const formData = new FormData();
+//     formData.append('document', event.target.document.files[0]); 
+//     formData.append('examId', selectedExam);
+//     formData.append('selectedSubjects', selectedSubjects); 
+
+//     try {
+//         const response = await axios.post('http://localhost:5000/ExamCreation/uploadDocument', formData, {
+//             headers: {
+//                 'Content-Type': 'multipart/form-data',
+//             },
+//         });
+//         console.log(response.data); // Log success message
+//         alert("Document Uploaded Succesfully")
+//         resetForm(); // Reset form after successful submission
+//     } catch (error) {
+//         console.error('Error uploading document:', error);
+//     }
+// };
+
+//   return (
+//     <div>
+//       <div className='headerjeem'>
+//         <div className='headerjee'>
+//           <img src={Logo_img} alt="Logo" />
+//         </div>
+//         <a className='jeeanchor' href='/Home'>
+//           <IoMdHome /> Home
+//         </a>
+//       </div>
+//       <Leftnavbar />
+//       <div className='headerpageh1'>
+//         <h1> Document Upload page</h1>
+//       </div>
+//       <button className='btnes' onClick={() => setModalOpen(true)}>Document Upload</button>
+
+//       {modalOpen && (
+//         <div className='examform'>
+//           <div className='modal'>
+//             <div className='content_s'>
+//               <SelectionForm
+//                 onSubmit={handleSubmit}
+//                 selectedExam={selectedExam}
+//                 onExamChange={handleExamChange}
+//                 subjects={subjects}
+//                 onSubjectChange={handleSubjectChange}
+//                 exams={exams}
+//               />
+//               <button className='closebutton' onClick={resetForm}><RxCross2 /></button>
+//             </div>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+
+//   );
+// };
+
+
+// const SelectionForm = ({
+//   onSubmit,
+//   selectedExam,
+//   onExamChange,
+//   subjects,
+//   selectedSubjects,
+//   onSubjectChange,
+//   editingSelection,
+//   exams
+// }) => (
+//   <form onSubmit={onSubmit} className='ECUploadForm'>
+//     <h1 className='ECUploadFormh1'>{editingSelection ? 'Edit Selection' : 'Exam Selection'}</h1>
+//     <div className='div1'>
+//       <label htmlFor="exam">Select Exam:</label>
+//       <select id="examcreation" className='dropdown' value={selectedExam} onChange={onExamChange}>
+//         <option value="">--Select an exam--</option>
+//         {exams.map(exam => (
+//           <option key={exam.exam_id} value={exam.exam_id}>{exam.exam_name}</option>
+//         ))}
+//       </select>
+//     </div>
+
+//     <div className='div1'>
+//       <label htmlFor="subject-select">Select Subjects:</label>
+//       <select
+//         id="subject-select"
+//         className='dropdown'
+//         // multiple
+//         value={selectedSubjects}
+//         onChange={onSubjectChange}
+//       >
+//          <option value="">--Select an subject--</option>
+//         {subjects.map(subject => (
+//           <option
+//             key={subject.subject_id}
+//             value={subject.subject_id}
+
+//           >
+//             {subject.subject_name}
+//           </option>
+//         ))}
+//       </select>
+//     </div>
+
+//     <div className='div1'>
+//       <label>Topic Name</label>
+//       <input type='text'  />
+
+//     </div>
+
+
+//     <div className='div1'>
+//       <label htmlFor="document-upload">Choose Document:</label>
+//       <input
+//       className='dropdown'
+//         type="file"
+//         id="document-upload"
+//         name="document"
+//         accept=".doc,.docx"
+//         required
+//       />
+//     </div>
+
+//     <button type="submit" className='uploadbutton'>Submit</button>
+//   </form>
+// );
+
+// export default ExamCreation;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './MainsForm.css';
@@ -13,55 +227,65 @@ const ExamCreation = () => {
   const [subjects, setSubjects] = useState([]);
   const [selectedExam, setSelectedExam] = useState('');
   const [selectedSubjects, setSelectedSubjects] = useState([]);
-
+  const [topicName, setTopicName] = useState('');
 
   useEffect(() => {
-    // Fetch exams from the server
     fetch('http://localhost:5000/ExamCreation/exams')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => setExams(data))
-        .catch(error => console.error('Error fetching exams:', error));
-}, []); // Empty dependency array means this runs once on component mount
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => setExams(data))
+      .catch(error => console.error('Error fetching exams:', error));
+  }, []);
 
-useEffect(() => {
+  useEffect(() => {
     if (selectedExam) {
-        // Fetch subjects for the selected exam
-        fetch(`http://localhost:5000/ExamCreation/subjectsForExams/${selectedExam}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => setSubjects(data))
-            .catch(error => console.error('Error fetching subjects:', error));
+      fetch(`http://localhost:5000/ExamCreation/subjectsForExams/${selectedExam}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => setSubjects(data))
+        .catch(error => console.error('Error fetching subjects:', error));
     } else {
-        setSubjects([]); // Reset subjects if no exam is selected
+      setSubjects([]);
+      setSelectedSubjects([]); // Clear selected subjects when exam is changed
+      setTopicName(''); // Reset topic name
     }
-}, [selectedExam]); // Runs when selectedExam changes
-
+  }, [selectedExam]);
 
   const handleExamChange = (event) => {
     setSelectedExam(event.target.value);
     setSelectedSubjects([]);
+    setTopicName(''); // Reset topic name when exam changes
   };
 
   const handleSubjectChange = (event) => {
-    console.log(event.target.value)
-    // const selectedOptions = Array.from(event.target.value);
-    setSelectedSubjects(event.target.value);
+    const selectedId = event.target.value;
+    setSelectedSubjects(selectedId);
+
+    // Assuming the subject name or any other relevant data you want to display as topic comes from the selected subject ID.
+    const selectedSubject = subjects.find(subject => subject.subject_id === selectedId);
+    if (selectedSubject) {
+      setTopicName(selectedSubject.topic_name || ''); // Set topic name based on selected subject
+    } else {
+      setTopicName(''); // Reset if no subject is selected
+    }
   };
-
-
+  const handleTopicChange = (event) => {
+    setTopicName(event.target.value); // Update topic name based on input
+  };
   const resetForm = () => {
     setModalOpen(false);
     setSelectedExam('');
     setSelectedSubjects([]);
+    setTopicName(''); // Reset topic name
+    console.log(topicName)
   };
 
   const handleSubmit = async (event) => {
@@ -69,23 +293,24 @@ useEffect(() => {
     event.preventDefault();
 
     const formData = new FormData();
-    formData.append('document', event.target.document.files[0]); 
+    formData.append('document', event.target.document.files[0]);
     formData.append('examId', selectedExam);
-    formData.append('selectedSubjects', selectedSubjects); 
+    formData.append('selectedSubjects', selectedSubjects);
+    formData.append('topicName', topicName); // Append topic name to form data
 
     try {
-        const response = await axios.post('http://localhost:5000/ExamCreation/uploadDocument', formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-            },
-        });
-        console.log(response.data); // Log success message
-        alert("Document Uploaded Succesfully")
-        resetForm(); // Reset form after successful submission
+      const response = await axios.post('http://localhost:5000/ExamCreation/uploadDocument', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log(response.data); // Log success message
+      alert("Document Uploaded Successfully")
+      resetForm(); // Reset form after successful submission
     } catch (error) {
-        console.error('Error uploading document:', error);
+      console.error('Error uploading document:', error);
     }
-};
+  };
 
   return (
     <div>
@@ -114,6 +339,8 @@ useEffect(() => {
                 subjects={subjects}
                 onSubjectChange={handleSubjectChange}
                 exams={exams}
+                topicName={topicName}
+                onTopicChange={handleTopicChange}
               />
               <button className='closebutton' onClick={resetForm}><RxCross2 /></button>
             </div>
@@ -121,23 +348,22 @@ useEffect(() => {
         </div>
       )}
     </div>
-    
   );
 };
-
 
 const SelectionForm = ({
   onSubmit,
   selectedExam,
   onExamChange,
-  subjects,
   selectedSubjects,
+  subjects,
   onSubjectChange,
-  editingSelection,
-  exams
+  exams,
+  topicName, // Receive topic name as a prop
+  onTopicChange // Receive onTopicChange prop
 }) => (
   <form onSubmit={onSubmit} className='ECUploadForm'>
-    <h1 className='ECUploadFormh1'>{editingSelection ? 'Edit Selection' : 'Exam Selection'}</h1>
+    <h1 className='ECUploadFormh1'>Exam Selection</h1>
     <div className='div1'>
       <label htmlFor="exam">Select Exam:</label>
       <select id="examcreation" className='dropdown' value={selectedExam} onChange={onExamChange}>
@@ -153,16 +379,14 @@ const SelectionForm = ({
       <select
         id="subject-select"
         className='dropdown'
-        // multiple
         value={selectedSubjects}
         onChange={onSubjectChange}
       >
-         <option value="">--Select an subject--</option>
+        <option value="">--Select a subject--</option>
         {subjects.map(subject => (
           <option
             key={subject.subject_id}
             value={subject.subject_id}
-
           >
             {subject.subject_name}
           </option>
@@ -170,11 +394,20 @@ const SelectionForm = ({
       </select>
     </div>
 
+    <div className='div1'>
+      <label>Topic Name:</label>
+      <input
+        type='text'
+        value={topicName} // Bind the input to topic name state
+        onChange={onTopicChange} // Add onChange handler here
+        className='topicNameEC'
+      />
+    </div>
 
     <div className='div1'>
       <label htmlFor="document-upload">Choose Document:</label>
       <input
-      className='dropdown'
+        className='dropdown'
         type="file"
         id="document-upload"
         name="document"
@@ -187,4 +420,7 @@ const SelectionForm = ({
   </form>
 );
 
+
 export default ExamCreation;
+
+
